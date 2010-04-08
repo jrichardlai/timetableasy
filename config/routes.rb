5ba56b9a082de68c0b4bc281cc4e010e7b356dd5
Bady <61316@supinfo.com>
2010-04-08 12:02:21 +0200

    Commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ ActionController::Routing::Routes.draw do |map|
   map.register '/register', :controller => 'users', :action => 'create'
   map.signup '/signup', :controller => 'users', :action => 'new'
   map.resources :users
+  
+  map.resources :events
   map.resources :events, :member => [:move, :resize]
 
   map.resource :session
@@ -49,7 +51,7 @@ ActionController::Routing::Routes.draw do |map|
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
   
-  map.root :controller => 'events'
+  map.root :controller => 'sessions', :action => 'new'
   
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
