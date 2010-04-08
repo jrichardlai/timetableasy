39c53d755ac1bf43391fdf870fbfab351149d1eb
Bady <61316@supinfo.com>
2010-04-08 17:19:16 +0200

    Ajout de l'I18n, fichiers yml.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,21 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   include AuthenticatedSystem
   
+  before_filter :set_locale
+  
+  def set_locale
+     #Dev purpose Only
+     logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+     #To delete ...
+     I18n.locale = extract_locale_from_accept_language_header
+     logger.debug "* Locale set to '#{I18n.locale}'"
+  end 
+     
+private 
+  def extract_locale_from_accept_language_header
+     request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end 
+  
   # Commentaire Test
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   # Be sure to include AuthenticationSystem in Application Controller instead
   include AuthenticatedSystem
-  
+  layout 'user'
 
   # render new.rhtml
   def new
