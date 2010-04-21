96a05453ea475f3507187d86746b82dc685b4724
xlrj <jrichard.lai@gmail.com>
2010-04-21 17:10:36 +0200

    Edited user edit and new actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,28 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  # PUT /cursuses/1
+  # PUT /cursuses/1.xml
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        flash[:notice] = 'User was successfully updated.'
+        format.html { redirect_to(cursus_path @user) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
   def index
     @users = User.all
     respond_to do |format|
