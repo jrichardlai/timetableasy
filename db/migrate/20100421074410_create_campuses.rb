754502df969383cc0dc724c60d47d2ed720fec1d
xlrj <jrichard.lai@gmail.com>
2010-04-21 09:49:52 +0200

    Removed campus migration.

--- db/migrate/20100421074410_create_campuses.rb
+++ db/migrate/20100421074410_create_campuses.rb
@@ -1,7 +1,12 @@
 class CreateCampuses < ActiveRecord::Migration
   def self.up
     create_table :campuses do |t|
-
+      t.string :name
+      t.text :address
+      t.string :city
+      t.string :zip
+      t.string :country
+      t.string :timezone
       t.timestamps
     end
   end
