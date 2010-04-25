9c5ec102dc47e2cfd2f38670fa04d74a11fc32c0
xlrj <jrichard.lai@gmail.com>
2010-04-25 23:23:24 +0200

    Add campus relations to classrooms

--- app/models/campus.rb
+++ app/models/campus.rb
@@ -4,6 +4,6 @@ class Campus < ActiveRecord::Base
   has_many :managements
   has_many :managers, :source => :user, :through => :managements
   # TODO
-  # has_many :classrooms
+  has_many :classrooms
   # has_many :rooms
 end
