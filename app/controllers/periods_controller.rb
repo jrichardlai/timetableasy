d6cd0af17c0f96e27b94c207d44abf377e268ef6
xlrj <jrichard.lai@gmail.com>
2010-05-10 16:25:41 +0200

    Edited periods.

--- app/controllers/periods_controller.rb
+++ app/controllers/periods_controller.rb
@@ -48,7 +48,7 @@ class PeriodsController < ApplicationController
     respond_to do |format|
       if @period.save
         flash[:notice] = 'Period was successfully created.'
-        format.html { redirect_to(cursuses_periods_path(@cursus, @period)) }
+        format.html { redirect_to(cursus_periods_path(@cursus, @period)) }
         format.xml  { render :xml => @period, :status => :created, :location => @period }
       else
         format.html { render :action => "new" }
--- app/models/period.rb
+++ app/models/period.rb
@@ -1,5 +1,5 @@
 class Period < ActiveRecord::Base
   belongs_to :period_type
   belongs_to :cursus
-  delegate :name, :to => :period_type
+  delegate :name, :to => :period_type, :allow_nil => true
 end
